d53968bbc1e2dbcca1359b804d42838bac44175d
Jhennifer-byt <118638742+Jhennifer-byt@users.noreply.github.com>
2024-09-16 15:11:26 -0500

    creación de carpetas

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import os
 app = Flask(__name__, template_folder='templates')
 
 # Configuración de la base de datos
-DB_HOST = 'dpg-cr6bdj1u0jms73bn1teg-a.oregon-postgres.render.com'
-DB_NAME = 'dbtest_h0hy'
-DB_USER = 'dbtest_h0hy_user'
-DB_PASSWORD = 'xkmD4V6rmoGNJ27uGLq1k76ynORQ8HTd'
+DB_HOST = 'dpg-crk8mc9u0jms73fjtl60-a'
+DB_NAME = 'examen_rlx0'
+DB_USER = 'examen_rlx0_user'
+DB_PASSWORD = 'MfoJ0gZif0ovWXMXxFEezYQtYeOIGnQV'
 
 
 def conectar_db():
